7a514544a2f604216f726cd585c562f333ee23e5
ayberkgerey <ayberkgerey@gmail.com>
2021-01-09 00:23:59 +0300

    Continue to deviceCard.

--- src/components/DeviceCard.js
+++ src/components/DeviceCard.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function DeviceCard() {
   return (
-    <View>
-      <Text>DeviceCard </Text>
+    <View style={styles.cardContainer}>
+      <View>
+        <Text style={styles.title}>DEVICENAME</Text>
+        <Text style={styles.codeTitle}>DEVICECODE</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 6,
+    height: 58,
+    width: '90%',
+    backgroundColor: '#3a3939',
+    borderColor: '#acee0f',
+    borderWidth: 0.9,
+  },
+  title: {
+    color: '#acee0f',
+    fontSize: 20,
+  },
+  codeTitle: {
+    color: '#e6e6e6',
+    fontSize: 12,
+  },
+});
